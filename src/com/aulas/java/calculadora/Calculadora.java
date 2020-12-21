68eb270c659ea7f8f5e5afb4e1d36eb510dde8f2
Sandro0330 <sandro030486@gmail.com>
2020-12-21 16:53:03 -0300

    Calculadora usando Enum

---
+++ src/com/aulas/java/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+package com.aulas.java.calculadora;
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+        double x = 2.0;
+        double y = 3.0;
+
+        for(Operacao op : Operacao.values()) {
+            System.out.print(x + " ");
+            System.out.print(op.toString() + " ");
+            System.out.print(y + " = ");
+            System.out.println(op.executarOperacao(x, y));
+        }
+    }
+}
